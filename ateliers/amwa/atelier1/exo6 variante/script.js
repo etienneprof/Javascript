3a95461d86f022a600e113ba309ee992013045d0
excilyano <agahnim13@gmail.com>
2025-01-03 12:19:25 +0100

    bonus deplacement diagonal

--- ateliers/amwa/atelier1/exo6 variante/script.js
+++ ateliers/amwa/atelier1/exo6 variante/script.js
@@ -3,9 +3,73 @@ const speed = 8;
 let x_position = character.offsetLeft;
 let y_position = character.offsetTop;
 
-window.addEventListener("keydown", move);
+let x_deplacement = 0;
+let y_deplacement = 0;
 
-function move(event) {
+window.addEventListener("keydown", onkeydown);
+window.addEventListener("keyup", onkeyup);
+
+setInterval(() => {
+    character.style.left = `${x_position}px`;
+    character.style.top = `${y_position}px`; 
+}, 1000/60);
+
+
+function onkeydown(event) {
+    console.log(`Clic sur la touche ${event.key}`);
+    switch(event.keyCode) {
+        case 37 :
+            x_deplacement = -speed;
+            break;
+        case 38 :
+            y_deplacement = -speed;
+            break;
+        case 39 :
+            x_deplacement = speed;
+            break;
+        case 40 :
+            y_deplacement = speed;
+            break;
+        default :
+            console.log("Déplacement invalide !");
+            break;
+    }
+    x_position += x_deplacement;
+    y_position += y_deplacement;
+}
+
+function onkeyup(event) {
+    console.log(`Relachement de la touche ${event.key}`);
+    switch(event.keyCode) {
+        case 37 :
+            x_deplacement = 0;
+            break;
+        case 38 :
+            y_deplacement = 0;
+            break;
+        case 39 :
+            x_deplacement = 0;
+            break;
+        case 40 :
+            y_deplacement = 0;
+            break;
+        default :
+            console.log("Déplacement invalide !");
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
     switch(event.keyCode) {
         case 37 :
             x_position-=speed;
@@ -25,4 +89,4 @@ function move(event) {
     }
     character.style.left = `${x_position}px`;
     character.style.top = `${y_position}px`;
-}
+    */
